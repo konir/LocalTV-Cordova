dfa39178072f3b3b2fa9c293cefcbae516b0ff02
konir <koniroth@gmail.com>
2013-10-08 20:58:42 +0200

    rename project to cordova

---
+++ www/js/util.js
@@ -0,0 +1,62 @@
+/* Get URL Parameter */
+function gup(name) {
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regexS = "[\\?&]" + name + "=([^&#]*)";
+	var regex = new RegExp(regexS);
+	var results = regex.exec(window.location.href);
+	if (results == null) {
+		return "";
+	} else {
+		return results[1];
+	}
+}
+
+function checkInternetConnection() {
+	var isOnLine = navigator.onLine;
+	if (isOnLine) {
+		// alert('connected');
+		return true;
+	} else {
+		alert('Sie sind nicht mit dem Internet verbunden!');
+		return false;
+	}
+}
+
+function doesConnectionExist() {
+	var xhr = new XMLHttpRequest();
+	// var file = "https://www.google.com/images/srpr/logo6w.png";
+	var file = "http://www.rothconsulting.com/images/rc_big.gif";
+	var randomNum = Math.round(Math.random() * 10000);
+
+	xhr.open('HEAD', file + "?rand=" + randomNum, false);
+
+	try {
+		xhr.send();
+
+		if (xhr.status >= 200 && xhr.status < 304) {
+			alert('Connected !!!');
+			return true;
+		} else {
+			alert('NOT Connected !!!');
+			return false;
+		}
+	} catch (e) {
+		alert('NOT Connected - with exception !!: ' + e);
+		return false;
+	}
+}
+
+function getCountry() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			$.getJSON('http://ws.geonames.org/countryCode', {
+				lat : position.coords.latitude,
+				lng : position.coords.longitude,
+				type : 'JSON'
+			}, function(result) {
+				// alert(result.countryName);
+				return result.countryName;
+			});
+		});
+	}
+}
